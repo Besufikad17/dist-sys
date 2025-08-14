f62fd6db26083be5fb8e773eb88b2cdbc08f59b4
Besufikad17 <besufikadtilahun1994@gmail.com>
2025-08-14 18:55:33 +0300

    mod: custom types added for unique-id-generator problem solution

--- unique-id-generation/main.go
+++ unique-id-generation/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"math/rand"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type Body struct {
+	Type      string `json:"type"`
+	MsgId     int    `json:"msg_id"`
+	InReplyTo int    `json:"in_reply_to"`
+}
+
+type Reply struct {
+	Type      string `json:"type"`
+	InReplyTo int    `json:"in_reply_to"`
+}
+
+type GenerateReply struct {
+	Reply
+	Id int `json:"id"`
+}
+
 func main() {
 	node := maelstrom.NewNode()
 
 	node.Handle("generate", func(msg maelstrom.Message) error {
-		var body map[string]any = map[string]any{
-			"type": "generate_ok",
-			"id":   rand.Int(),
+		var body Body
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
 		}
 
-		return node.Reply(msg, body)
+		reply := GenerateReply{
+			Reply: Reply{
+				Type:      "generate_ok",
+				InReplyTo: body.InReplyTo,
+			},
+			Id: rand.Int(),
+		}
+
+		return node.Reply(msg, reply)
 	})
 
 	if err := node.Run(); err != nil {
